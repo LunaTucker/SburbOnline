74e515f10c3655a1277c702c9ba47c66a0c66be4
Luna Tucker <lunaskywalker37@gmail.com>
2019-08-14 20:42:56 -0700

    Removed unneeded old backups

--- _backups/4/assets/maps/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="64" tileheight="64" tilecount="2" columns="2">
- <image source="tiles.png" width="128" height="64"/>
-</tileset>
