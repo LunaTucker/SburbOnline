bdb2ebcf058da36e89d00775b088ccaffa22c307
Luna Tucker <lunaskywalker37@gmail.com>
2019-08-21 15:36:22 -0700

    added loading screen for preload assets

--- js/main.js
+++ js/main.js
@@ -11,6 +11,47 @@ Phaser.Scene.call(this,{key:'Main', active: true});
 },
     
 preload: function() { 
+
+//LOADING SCREEN
+    //create the screen
+      //box
+  var progressBar = this.add.graphics();
+  var progressBox = this.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(240, 270, 320, 50);
+      //text
+  var width = this.cameras.main.width;
+  var height = this.cameras.main.height;
+  var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+          font: '20px monospace',
+          fill: '#ffffff'
+      }
+  });
+loadingText.setOrigin(0.5, 0.5);
+
+
+    //update the progress as we go
+  this.load.on('progress', function (value) {
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+  });
+            
+this.load.on('fileprogress', function (file) {
+   // console.log(file.src);
+});
+
+ //remove loading screen
+this.load.on('complete', function () {	
+  loadingText.destroy();
+  progressBar.destroy();
+  progressBox.destroy();
+});
+
     this.load.animation('karkatData', 'assets/characters/karkat_anim.json');
     this.load.atlas('karkat', 'assets/characters/karkat.png', 'assets/characters/karkat_atlas.json');
     
